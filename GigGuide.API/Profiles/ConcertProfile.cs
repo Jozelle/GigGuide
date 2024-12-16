ec72f4136b4cf9206ca5c0a1b54591932faed0a9
Jozelle <johannagull92@gmail.com>
2024-12-16 11:54:39 +0100

    Started with API and first profile.

---
+++ GigGuide.API/Profiles/ConcertProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GigGuide.Data.DTO;
+using GigGuide.Data.Entities;
+
+namespace GigGuide.API.Profiles
+{
+    public class ConcertProfile : Profile   
+    {
+        public ConcertProfile()
+        {
+            CreateMap<Concert, ConcertDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id));
+
+            CreateMap<ConcertDto, Concert>();
+        }
+    }
+}
---
+++ GigGuide.Data.DTO/VenueDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigGuide.Data.DTO
+{
+    public class VenueDto
+    {
+        public string? Id { get; set; } = null!;
+        public string? Name { get; set; } = null!;
+        public string? Address { get; set; } = null!;
+        public string? City { get; set; } = null!;
+        public string? Country { get; set; } = null!;
+    }
+}
