2f1fa49fe462089bfa70d91033a4eac7c6344eb5
Jozelle <johannagull92@gmail.com>
2024-12-16 11:39:47 +0100

    Need to check how to deal with DTOs and linked objects.

---
+++ GigGuide.Data.DTO/ConcertDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigGuide.Data.DTO
+{
+    public class ConcertDto
+    {
+        public int? Id { get; set; } = null!;
+        public string Artist { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public ICollection<PerformanceDto> Performances { get; set; } = null!;
+    }
+}
---
+++ GigGuide.Data.DTO/PerformanceDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigGuide.Data.DTO
+{
+    public class PerformanceDto
+    {
+        public int? Id { get; set; } = null!;
+        public DateTime? PerformanceTime { get; set; } = null!;
+        public int? TicketPrice { get; set; } = null!;
+        public int? TicketsAvailable { get; set; } = null!;
+        public int? ConcertId { get; set; } = null!;
+        public int? VenueId { get; set; } = null!;
+    }
+}
