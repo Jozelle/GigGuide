6c24bae35242c759eac4fcf45b8fb0552de9f3d5
Jozelle <johannagull92@gmail.com>
2024-12-17 20:50:38 +0100

    Bugfix

--- GigGuide.MAUI/Profiles/CustomerProfile.cs
+++ GigGuide.MAUI/Profiles/CustomerProfile.cs
@@ -12,15 +12,15 @@ namespace GigGuide.MAUI.Profiles
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.CustomerId))
                 .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.CustomerFirstName))
                 .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.CustomerLastName))
-                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.CustomerEmail))
-                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.CustomerPhone));
+                .ForMember(dest => dest.EmailAddress, opt => opt.MapFrom(src => src.CustomerEmail))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.CustomerPhone));
 
             CreateMap<CustomerDto, Customer>()
                 .ForMember(dest => dest.CustomerId, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.CustomerFirstName, opt => opt.MapFrom(src => src.FirstName))
                 .ForMember(dest => dest.CustomerLastName, opt => opt.MapFrom(src => src.LastName))
-                .ForMember(dest => dest.CustomerEmail, opt => opt.MapFrom(src => src.Email))
-                .ForMember(dest => dest.CustomerPhone, opt => opt.MapFrom(src => src.Phone));
+                .ForMember(dest => dest.CustomerEmail, opt => opt.MapFrom(src => src.EmailAddress))
+                .ForMember(dest => dest.CustomerPhone, opt => opt.MapFrom(src => src.PhoneNumber));
         }
     }
 }
