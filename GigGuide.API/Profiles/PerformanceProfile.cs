d0132efe4a692d874fdb7c73a9a01a9c768df2fb
carolinebarso <70707494+CarolineBarso@users.noreply.github.com>
2024-12-18 16:43:15 +0100

    Profiles klara

---
+++ GigGuide.API/Profiles/PerformanceProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using GigGuide.Data.DTO;
+using GigGuide.Data.Entities;
+
+namespace GigGuide.API.Profiles
+{
+    public class PerformanceProfile : Profile   
+    {
+        public PerformanceProfile() 
+        {
+            CreateMap<Performance, PerformanceDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.PerformanceTime, opt => opt.MapFrom(src => src.PerformanceTime))
+                .ForMember(dest => dest.TicketPrice, opt => opt.MapFrom(src => src.TicketPrice))
+                .ForMember(dest => dest.TicketsAvailable, opt => opt.MapFrom(src => src.TicketsAvailable))
+                .ForMember(dest => dest.ConcertId, opt => opt.MapFrom(src => src.ConcertId))
+                .ForMember(dest => dest.VenueId, opt => opt.MapFrom(src => src.VenueId))
+                ;
+
+            CreateMap<PerformanceDto, Performance>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.PerformanceTime, opt => opt.MapFrom(src => src.PerformanceTime))
+                .ForMember(dest => dest.TicketPrice, opt => opt.MapFrom(src => src.TicketPrice))
+                .ForMember(dest => dest.TicketsAvailable, opt => opt.MapFrom(src => src.TicketsAvailable))
+                .ForMember(dest => dest.ConcertId, opt => opt.MapFrom(src => src.ConcertId))
+                .ForMember(dest => dest.VenueId, opt => opt.MapFrom(src => src.VenueId))
+                ;
+        }
+    }
+}
