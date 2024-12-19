8053b51b20d4bdbbbffab5e7fe26dcf87a916441
Jozelle <johannagull92@gmail.com>
2024-12-19 20:13:20 +0100

    Skapat upp model, service och page för Booking.

---
+++ GigGuide.MAUI/Services/BookingService.cs
@@ -0,0 +1,38 @@
+﻿using GigGuide.MAUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigGuide.MAUI.Services
+{
+    public class BookingService : IBookingService
+    {
+        IRestService _restService;
+
+        public BookingService(IRestService restService)
+        {
+            _restService = restService;
+        }
+        public Task<Booking?> GetBookingAsync(int bookingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Booking>?> GetBookingsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Booking>?> GetBookingsByCustomerAsync(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Booking>?> GetBookingsByPerformanceAsync(int performanceId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GigGuide.MAUI/Services/IBookingService.cs
@@ -0,0 +1,17 @@
+﻿using GigGuide.MAUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigGuide.MAUI.Services
+{
+    public interface IBookingService
+    {
+        Task<List<Booking>?> GetBookingsAsync();
+        Task<List<Booking>?> GetBookingsByCustomerAsync(int customerId);
+        Task<List<Booking>?> GetBookingsByPerformanceAsync(int performanceId);
+        Task<Booking?> GetBookingAsync(int bookingId);
+    }
+}
