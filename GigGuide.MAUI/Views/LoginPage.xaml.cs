cba00a2664c9edabb771a524f203789219aeca5f
carolinebarso <70707494+CarolineBarso@users.noreply.github.com>
2025-01-02 14:29:07 +0100

    swipe gestures still not disabled

--- GigGuide.MAUI/Views/LoginPage.xaml.cs
+++ GigGuide.MAUI/Views/LoginPage.xaml.cs
@@ -7,11 +7,20 @@ namespace GigGuide.MAUI.Views
         public LoginPage()
         {
             InitializeComponent();
+            Shell.SetNavBarIsVisible(this, false); // Hides the back arrow
+            Shell.SetTabBarIsVisible(this, false); // Hides the bottom navigation bar
         }
 
         public LoginPage(LoginViewModel viewModel) : this()
         {
             BindingContext = viewModel;
         }
+        protected override bool OnBackButtonPressed()
+        {
+            // Prevent the back button from being used
+            return true;
+        }
+
+
     }
 }
