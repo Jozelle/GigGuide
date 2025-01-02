c9b9c91c87c4d61f35fec4d23ff5ce71592b7995
carolinebarso <70707494+CarolineBarso@users.noreply.github.com>
2025-01-02 14:29:07 +0100

    swipe gestures still not disabled

--- GigGuide.MAUI/Views/ConcertListPage.xaml.cs
+++ GigGuide.MAUI/Views/ConcertListPage.xaml.cs
@@ -1,13 +1,24 @@
 using GigGuide.MAUI.ViewModels;
+using System.Diagnostics;
 
 namespace GigGuide.MAUI.Views;
 
 public partial class ConcertListPage : ContentPage
 {
+    
     public ConcertListPage(ConcertListViewModel viewModel)
     {
         InitializeComponent();
         BindingContext = viewModel;
         Shell.SetBackButtonBehavior(this, new BackButtonBehavior { IsEnabled = false });
+    }
+
+    private  void OnSwiped(object sender, SwipedEventArgs e)
+    {
+        Debug.WriteLine($"Swipe detected: {e.Direction}");
+        if (e.Direction == SwipeDirection.Left)
+        {
+             DisplayAlert("Swipe Detected", "Back navigation is disabled.", "OK");
+        }
     }
 }
