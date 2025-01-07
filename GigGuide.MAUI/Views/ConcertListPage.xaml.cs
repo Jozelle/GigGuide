7016cbb30edc4690f41e06d8305311930dbe51f0
carolinebarso <70707494+CarolineBarso@users.noreply.github.com>
2025-01-07 11:15:32 +0100

    fix in xaml

--- GigGuide.MAUI/Views/ConcertListPage.xaml.cs
+++ GigGuide.MAUI/Views/ConcertListPage.xaml.cs
@@ -10,15 +10,16 @@ public partial class ConcertListPage : ContentPage
     {
         InitializeComponent();
         BindingContext = viewModel;
-        Shell.SetBackButtonBehavior(this, new BackButtonBehavior { IsEnabled = false });
+        //Shell.SetBackButtonBehavior(this, new BackButtonBehavior { IsEnabled = false });
+        Shell.SetNavBarIsVisible(this, false); // Hides the back arrow
     }
 
-    private  void OnSwiped(object sender, SwipedEventArgs e)
-    {
-        Debug.WriteLine($"Swipe detected: {e.Direction}");
-        if (e.Direction == SwipeDirection.Left)
-        {
-             DisplayAlert("Swipe Detected", "Back navigation is disabled.", "OK");
-        }
-    }
+    //private  void OnSwiped(object sender, SwipedEventArgs e)
+    //{
+    //    Debug.WriteLine($"Swipe detected: {e.Direction}");
+    //    if (e.Direction == SwipeDirection.Left)
+    //    {
+    //         DisplayAlert("Swipe Detected", "Back navigation is disabled.", "OK");
+    //    }
+    //}
 }
