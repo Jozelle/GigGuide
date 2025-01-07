8104485151341706a7bfff9088bb597c63131fea
Jozelle <johannagull92@gmail.com>
2025-01-07 16:38:55 +0100

    Small clean

--- GigGuide.MAUI/Views/LoginPage.xaml.cs
+++ GigGuide.MAUI/Views/LoginPage.xaml.cs
@@ -4,16 +4,12 @@ namespace GigGuide.MAUI.Views
 {
     public partial class LoginPage : ContentPage
     {
-        public LoginPage()
+        public LoginPage(LoginViewModel viewModel)
         {
             InitializeComponent();
-            Shell.SetNavBarIsVisible(this, false); // Hides the back arrow
-            Shell.SetTabBarIsVisible(this, false); // Hides the bottom navigation bar
-        }
-
-        public LoginPage(LoginViewModel viewModel) : this()
-        {
             BindingContext = viewModel;
+            //Shell.SetNavBarIsVisible(this, false); // Hides the back arrow
+            //Shell.SetTabBarIsVisible(this, false); // Hides the bottom navigation bar
         }
     }
 }
